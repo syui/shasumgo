689961db39fdb07b8f780dbebf773b8c9b14c440
syui <syui@syui.cf>
2020-03-05 12:35:58 +0900

    first

---
+++ readme.md
@@ -0,0 +1,13 @@
+`checksum` go
+
+```sh
+$ shasumgo ~/.bashrc ~/.bashrc
+ok
+
+$ shasumgo ~/.bashrc ~/.zshrc
+da39a3ee5e6b4b0d3255bfef95601890afd80709 336de4a66576637c35e0a02e3b4233fdb28798db
+
+# success:result display is none
+$ shasumgo c ~/.bashrc ~/.bashrc
+
+```
---
+++ shasumgo.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"crypto/md5"
+	"crypto/sha1"
+	"crypto/sha256"
+	"crypto/sha512"
+	"errors"
+	"flag"
+	"fmt"
+	"os"
+	"io/ioutil"
+	"strings"
+	"github.com/urfave/cli/v2"
+)
+
+func App() *cli.App {
+	app := cli.NewApp()
+	app.Name = "shasumgo"
+	app.Usage = "shasumgo .bashrc .zshrc"
+	return app
+}
+
+func Action(c *cli.Context) {
+	app := App()
+	if c.Args().Get(0) == "" {
+		help := []string{"", "--help"}
+		app.Run(help)
+		os.Exit(1)
+	}
+	return
+}
+
+var (
+	algorithm = flag.String("a", "1", "algorithm md5, 1, 224, 256, 384, 512, 512224, 512256")
+)
+
+func shasum(data *[]byte, algorithm string) (interface{}, error) {
+	var sum interface{}
+	switch strings.ToLower(algorithm) {
+	case "md5":
+		sum = md5.Sum(*data)
+	case "1":
+		sum = sha1.Sum(*data)
+	case "224":
+		sum = sha256.Sum224(*data)
+	case "256":
+		sum = sha256.Sum256(*data)
+	case "384":
+		sum = sha512.Sum384(*data)
+	case "512":
+		sum = sha512.Sum512(*data)
+	case "512224":
+		sum = sha512.Sum512_224(*data)
+	case "512256":
+		sum = sha512.Sum512_256(*data)
+	default:
+		return nil, errors.New("unsupported algorithm")
+	}
+	return sum, nil
+}
+
+func main() {
+	app := &cli.App{
+		Version: "0.1.0",
+		Name: "shasumgo",
+		Usage: "$ shasumgo go.mod go.sum",
+		Action: func(c *cli.Context) error {
+			if c.Args().Get(0) == "" {
+				help := []string{"shasumgo", "--help"}
+				fmt.Printf("%s", help)
+			} else if c.NArg() > 1 {
+				filea, _ := ioutil.ReadFile(c.Args().Get(0))
+				suma, err := shasum(&filea, *algorithm)
+				if err != nil {
+					fmt.Println(err)
+				}
+				fileb, _ := ioutil.ReadFile(c.Args().Get(1))
+				sumb, err := shasum(&fileb, *algorithm)
+				if err != nil {
+					fmt.Println(err)
+				}
+				if  suma == sumb {
+					fmt.Printf("ok")
+				} else {
+					fmt.Printf("%x %x", suma, sumb)
+				}
+			} else {
+				file, _ := ioutil.ReadFile(c.Args().Get(0))
+				sum, err := shasum(&file, *algorithm)
+				if err != nil {
+					fmt.Println(err)
+				}
+				fmt.Printf("%x", sum)
+				//fmt.Printf("%x\t%s\n", sum, file)
+			}
+			return nil
+		},
+	}
+	app.Commands = []*cli.Command{
+		{
+			Name:    "check",
+			Aliases: []string{"c"},
+			Usage:   "shasumgo c ./go.mod ./go.mod [success:result display is none]",
+			Action:  func(c *cli.Context) error {
+				filea, _ := ioutil.ReadFile(c.Args().Get(0))
+				suma, err := shasum(&filea, *algorithm)
+				if err != nil {
+					fmt.Println(err)
+				}
+				fileb, _ := ioutil.ReadFile(c.Args().Get(1))
+				sumb, err := shasum(&fileb, *algorithm)
+				if err != nil {
+					fmt.Println(err)
+				}
+				if  suma != sumb {
+					fmt.Printf("%x %x", suma, sumb)
+				}
+				return nil
+			},
+		},
+	}
+	app.Run(os.Args)
+}
+
